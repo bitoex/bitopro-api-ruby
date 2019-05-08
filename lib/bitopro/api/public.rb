48cecfe73e4b1baadd557bae0b39c1de6de72854
Nic <niclin0226@gmail.com>
2019-05-08 15:23:11 +0800

    Replace ticker to ticker's'

--- lib/bitopro/api/public.rb
+++ lib/bitopro/api/public.rb
@@ -8,10 +8,10 @@ module Bitopro
       get("/order-book/#{currency_pair}")
     end
 
-    def ticker(currency_pair)
+    def tickers(currency_pair)
       raise Error, "currency_pair is required" unless currency_pair
 
-      get("/ticker/#{currency_pair}")
+      get("/tickers/#{currency_pair}")
     end
 
     def recent_trades(currency_pair)
